c73c2ef30de6fa5b88e2a289a30c840304f144e4
marcos sanchez <marcos.visualnt@gmail.com>
2015-11-15 14:52:09 +0100

    Initial commit

---
+++ app/src/main/java/it/escuela/lexdroid2/clase2/lexcode2_03_fragment/MainActivity.java
@@ -0,0 +1,13 @@
+package it.escuela.lexdroid2.clase2.lexcode2_03_fragment;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
