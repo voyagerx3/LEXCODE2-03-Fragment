9c579a266a97332be283a72792510ca02ce05e0b
marcos sanchez <marcos.visualnt@gmail.com>
2015-11-15 21:42:27 +0100

    Paso 6 - Creamos interfaz para comunicar fragment

---
+++ app/src/main/java/it/escuela/lexdroid2/clase2/lexcode2_03_fragment/Enviar.java
@@ -0,0 +1,9 @@
+package it.escuela.lexdroid2.clase2.lexcode2_03_fragment;
+
+/**
+ * Created by marcos on 15/11/2015.
+ */
+public interface Enviar {
+    // creamos interfaz para comunicar fragment. solo contiene metodos abstractos
+    public void enviarTexto(String mensaje );
+}
