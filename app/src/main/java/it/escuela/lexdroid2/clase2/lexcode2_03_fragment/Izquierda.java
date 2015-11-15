baee559268e952d5f45013d8bec5793342bfa835
marcos sanchez <marcos.visualnt@gmail.com>
2015-11-15 21:58:51 +0100

    Paso 6.1 lado izq preparado para enviar

--- app/src/main/java/it/escuela/lexdroid2/clase2/lexcode2_03_fragment/Izquierda.java
+++ app/src/main/java/it/escuela/lexdroid2/clase2/lexcode2_03_fragment/Izquierda.java
@@ -1,5 +1,6 @@
 package it.escuela.lexdroid2.clase2.lexcode2_03_fragment;
 
+import android.app.Activity;
 import android.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -15,13 +16,41 @@ public class Izquierda extends Fragment {
     View rootView;
     Button boton;
     EditText text;
+    Enviar ENVIAR;
 
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle saveInstanceState){
-        rootView = inflater.inflate(R.layout.izquierda,container,false);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle saveInstanceState) {
+        rootView = inflater.inflate(R.layout.izquierda, container, false);
 
         text = (EditText) rootView.findViewById(R.id.text);
         boton = (Button) rootView.findViewById(R.id.boton);
 
+        boton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String mensaje;
+                mensaje = text.getText().toString();
+                //utilizamos la interfaz
+                ENVIAR.enviarTexto(mensaje);
+            }
+        });
+
+
         return rootView;
     }
+        // ciclo vida fragment .
+    public void onAttach(Activity activity){
+        super.onAttach(activity);
+
+        try{
+            ENVIAR = (Enviar) activity;
+        }
+        catch (ClassCastException e ){
+            throw new ClassCastException("necesitas el msg");
+        }
+
+
+    }
+
+
+
 }
