3727af571e1cbde2f1e3b632c446f89c9954b4ee
marcos sanchez <marcos.visualnt@gmail.com>
2015-11-15 15:24:36 +0100

    Paso 4, creamos clase izquierda.java

---
+++ app/src/main/java/it/escuela/lexdroid2/clase2/lexcode2_03_fragment/Derecha.java
@@ -0,0 +1,9 @@
+package it.escuela.lexdroid2.clase2.lexcode2_03_fragment;
+
+import android.app.Fragment;
+
+/**
+ * Created by marcos on 15/11/2015.
+ */
+public class Derecha extends Fragment{
+}
---
+++ app/src/main/java/it/escuela/lexdroid2/clase2/lexcode2_03_fragment/Izquierda.java
@@ -0,0 +1,27 @@
+package it.escuela.lexdroid2.clase2.lexcode2_03_fragment;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+
+/**
+ * Created by marcos on 15/11/2015.
+ */
+public class Izquierda extends Fragment {
+    View rootView;
+    Button boton;
+    EditText text;
+
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle saveInstanceState){
+        rootView = inflater.inflate(R.layout.izquierda,container,false);
+
+        text = (EditText) rootView.findViewById(R.id.text);
+        boton = (Button) rootView.findViewById(R.id.boton);
+
+        return rootView;
+    }
+}
