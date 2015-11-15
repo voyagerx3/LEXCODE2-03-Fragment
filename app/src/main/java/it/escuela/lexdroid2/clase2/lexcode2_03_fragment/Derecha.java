96ca9702d75c28527866a122b556d4f65481a744
marcos sanchez <marcos.visualnt@gmail.com>
2015-11-15 21:25:56 +0100

    Paso 5, creamos clase Derecha.java

--- app/src/main/java/it/escuela/lexdroid2/clase2/lexcode2_03_fragment/Derecha.java
+++ app/src/main/java/it/escuela/lexdroid2/clase2/lexcode2_03_fragment/Derecha.java
@@ -1,9 +1,27 @@
 package it.escuela.lexdroid2.clase2.lexcode2_03_fragment;
 
 import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
 
 /**
  * Created by marcos on 15/11/2015.
  */
 public class Derecha extends Fragment{
+
+   View rootView;
+    TextView txt;
+
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle saved saveInstanceState){
+
+        rootView = inflater.inflate(R.layout.izquierda,container,false);
+
+        txt = (TextView) rootView.findViewById(R.id.txt);
+
+        return rootView;
+    }
+
 }
